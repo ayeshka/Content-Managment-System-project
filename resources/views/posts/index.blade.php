780101556c7e5c7a4168136fbb7da496a43f58df
Sasiru <ayeshkasasiru1996@gmail.com>
2019-12-11 15:28:09 +0530

    Create Post index view , create view , PostsController, and create posts route in webapi

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+<div class="div d-flex justify-content-end mb-2">
+<a href="{{ route('posts.create')}}" class="btn btn-success ">Add Posts</a>
+</div>
+
+
+
+@endsection
