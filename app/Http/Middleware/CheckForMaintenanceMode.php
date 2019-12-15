b2d02cb73efcc862b28595c85d0467db71e28887
Sasiru <ayeshkasasiru1996@gmail.com>
2019-12-15 10:05:33 +0530

    CheckForMaintenanceMode middleware

--- app/Http/Middleware/CheckForMaintenanceMode.php
+++ app/Http/Middleware/CheckForMaintenanceMode.php
@@ -9,6 +9,13 @@ class CheckForMaintenanceMode extends Middleware
     /**
      * The URIs that should be reachable while maintenance mode is enabled.
      *
+     * This CheckForMaintenance Middlewre is an inbuild middlewre that check if if the application is on the maintanance before handaling any requests.
+     * php artisan down -> Put the application into maintenance mode
+     * php artisan up -> Bring the application out of maintenance mode
+     *
+     * What is middleware does before it handles any request it goes ahead on checks application and maintenance mode and
+     * if it's in maintenance mode then it is going to prevent the user from visiting that page or using that specific action
+     *
      * @var array
      */
     protected $except = [
